35693b602eb94c57be6a6ac43e95808719703264
欧阳培 <wb-oyp901093@alibaba-inc.com>
2021-12-20 16:44:09 +0800

    fix: 增加winOpen方法

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,23 @@ const linkTo = (puth: string) => {
   console.log(puth)
 }
 
+const winOpen = (url: string) => {
+  let openShow = false;
+  try {
+    const openInit = window.open(url);
+    if (openInit === null) {
+      openShow = true;
+    }
+  } catch (e) {
+    console.log(e);
+    openShow = true;
+  }
+  if (openShow) {
+    console.error('请先确认网站是否被拦截，如果是请关闭网站拦截');
+  }
+}
 
 export {
-  linkTo
+  linkTo,
+  winOpen
 }
